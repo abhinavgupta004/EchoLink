88caa51dd0cb5fa6f298c9178268da34c27e2d12
abhinavgupta004 <abhimanyurock.2004@gmail.com>
2026-02-04 20:16:21 +0530

    Fix Schema import in meeting model

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,7 +1,5 @@
 import express from "express";
 import {createServer} from "http";
-
-import {Server} from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import ConnectToSocket from "./controllers/socketManager.js";
@@ -11,7 +9,7 @@ import userRoutes from "./routes/users.routes.js";
 
 
 const app = express();
-const server = createServer(app);
+const server = createServer(app); 
 const io = ConnectToSocket(server);
 
 app.set("port", (process.env.PORT || 8080))
@@ -20,10 +18,10 @@ app.use(express.json({limit : "40kb"}))
 app.use(express.urlencoded({limit: "40kb", extended: true}))
 app.use("/api/v1/users", userRoutes);
 const start = async () => {
-    const connectionDb = mongoose.connect("mongodb+srv://abhimanyurock2004_db_user:CiCJYgos8LQZRQxG@cluster0.6qdrdif.mongodb.net/?appName=Cluster0")
+    const connectionDb = mongoose.connect("mongodb+srv://ankitmait64_db_user:R1GiiXDjQcVyreMg@cluster0.pp4n2vz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     console.log(`MONGO Connected DB Host : ${(await connectionDb).connection.host}`)
 server.listen(app.get("port"), (req,res)=> {
-    console.log("port is listening")
+    console.log("port is listening",app.get("port"))
 })
 }
 
